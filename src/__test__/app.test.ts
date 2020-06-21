53c0aa4189582b2276da6079905eadbf35dddbc3
Grzegorz Kazana <kazana.grzegorz@gmail.com>
2020-06-21 08:42:42 +0200

    test: e2e book module

--- src/__test__/app.test.ts
+++ src/__test__/app.test.ts
@@ -1,10 +1,11 @@
 import type { Author } from '../rest/common/author/Author.models';
+import type { Book, BookProps } from '../rest/common/book/Book.models';
 
 import request from 'supertest';
 import { Express } from 'express';
 
 import App from '../app';
-import { mockAuthor } from './mocks';
+import { mockAuthor, mockBooksByAuthor, mockBooks } from './mocks';
 
 /* eslint @typescript-eslint/no-unsafe-member-access: 0,
     @typescript-eslint/unbound-method: 0,
@@ -28,6 +29,32 @@ describe('app functionality', () => {
                             })
                             .then(({ body }) => body.author),
                     ),
+                );
+
+            const insertBook = (app: Express) => (book: BookProps): Promise<Book> =>
+                request(app)
+                    .post(`/${repo}/book`)
+                    .send(book)
+                    .then(({ body }) => body.book);
+
+            const insertBooksByAuthor = (app: Express) => (
+                author: Author,
+                books: BookProps[],
+            ): Promise<Book[]> =>
+                Promise.all(books.map(book => insertBook(app)({ ...book, author: author.id })));
+
+            const insertAuthorsAndBooks = (
+                app: Express,
+            ): Promise<{ books: Book[]; authors: Author[] }> =>
+                insertAuthors(app).then(authors =>
+                    Promise.all(
+                        authors.map(author =>
+                            insertBooksByAuthor(app)(author, mockBooksByAuthor[author.surname]),
+                        ),
+                    ).then(booksNested => ({
+                        authors,
+                        books: ([] as Book[]).concat(...booksNested),
+                    })),
                 );
 
             describe('in author module', () => {
@@ -127,6 +154,127 @@ describe('app functionality', () => {
                         .patch(`/${repo}/author/asd`)
                         .send({ name: 'asd' })
                         .expect(404));
+            });
+
+            describe('in book module', () => {
+                it('allows for insering and fetching data', async () => {
+                    const app = getApp();
+                    await insertAuthorsAndBooks(app);
+
+                    const { books } = await request(app)
+                        .get(`/${repo}/book`)
+                        .then(({ body }) => body);
+
+                    expect(books).toHaveLength(mockBooks.length);
+                    expect(books).toEqual(
+                        expect.arrayContaining(
+                            mockBooks.map(mockBook =>
+                                expect.objectContaining({
+                                    id: expect.any(String),
+                                    title: mockBook.title,
+                                    genre: mockBook.genre,
+                                }),
+                            ),
+                        ),
+                    );
+                });
+
+                it('allows for getting book by id', async () => {
+                    const app = getApp();
+                    const [insertedBook] = (await insertAuthorsAndBooks(app)).books;
+
+                    const { book } = await request(app)
+                        .get(`/${repo}/book/${insertedBook.id}`)
+                        .then(({ body }) => body);
+
+                    expect(book).toEqual(insertedBook);
+                });
+
+                it('returns 404 if book with id does not exist', () =>
+                    request(getApp()).get(`/${repo}/book/asd`).expect(404));
+
+                it('returns books by authors', async () => {
+                    const app = getApp();
+                    const { authors } = await insertAuthorsAndBooks(app);
+
+                    const orwellId = authors.find(({ surname }) => surname === 'Orwell')?.id || '';
+
+                    const { books: orwellBooks } = await request(app)
+                        .get(`/${repo}/book`)
+                        .query({ authorId: orwellId })
+                        .then(({ body }) => body);
+
+                    expect(orwellBooks).toHaveLength(mockBooksByAuthor.Orwell.length);
+                    expect(orwellBooks).toEqual(
+                        expect.arrayContaining(
+                            mockBooksByAuthor.Orwell.map(book =>
+                                expect.objectContaining({
+                                    title: book.title,
+                                    genre: book.genre,
+                                    author: orwellId,
+                                }),
+                            ),
+                        ),
+                    );
+                });
+
+                it('return books by genre', async () => {
+                    const app = getApp();
+                    await insertAuthorsAndBooks(app);
+
+                    const fantasyBooks = mockBooks.filter(({ genre }) => genre === 'Fantasy');
+
+                    const { books } = await request(app)
+                        .get(`/${repo}/book`)
+                        .query({ genre: 'fantasy' })
+                        .then(({ body }) => body);
+
+                    expect(books).toHaveLength(fantasyBooks.length);
+                    expect(books).toEqual(
+                        expect.arrayContaining(
+                            fantasyBooks.map(book =>
+                                expect.objectContaining({
+                                    title: book.title,
+                                    genre: book.genre,
+                                }),
+                            ),
+                        ),
+                    );
+                });
+
+                it('supports pagination', async () => {
+                    const app = getApp();
+                    const { books } = await insertAuthorsAndBooks(app);
+
+                    const { books: paginatedBooks } = await request(app)
+                        .get(`/${repo}/book`)
+                        .query({ from: 2, count: 3 })
+                        .then(({ body }) => body);
+
+                    expect(paginatedBooks).toHaveLength(3);
+                    expect(paginatedBooks).toEqual(
+                        expect.arrayContaining(
+                            books.slice(2, 5).map(book =>
+                                expect.objectContaining({
+                                    title: book.title,
+                                    genre: book.genre,
+                                }),
+                            ),
+                        ),
+                    );
+                });
+
+                it('handles invalid pagination', async () => {
+                    const app = getApp();
+                    await insertAuthorsAndBooks(app);
+
+                    const { books: paginatedBooks } = await request(app)
+                        .get(`/${repo}/book`)
+                        .query({ from: 0, count: -2 })
+                        .then(({ body }) => body);
+
+                    expect(paginatedBooks).toHaveLength(0);
+                });
             });
         });
     });
