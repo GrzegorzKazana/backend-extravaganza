dea6d3b8f3960cf657078f82e8d74759c62be73a
Grzegorz Kazana <kazana.grzegorz@gmail.com>
2020-07-02 07:12:45 +0200

    fix: app initialization

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import App from './app';
 const port = 4000;
 const app = new App(port);
 
-app.listen()
+app.init()
+    .then(app => app.listen())
     .then(() => console.log(`Server running on port ${port}`))
     .catch(() => console.log('Failed to initialize the app'));
